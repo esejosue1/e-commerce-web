e662ec7bf3306c7368fab74772bc98b18999856d
josue g <josuegallardolara@hotmail.com>
2022-07-13 14:48:31 -0700

    included import functionality for images in the category

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -129,3 +129,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     'ecommerce/static',
 ]
+
+#media files configuraions
+MEDIA_URL='/media/'
+MEDIA_ROOT = BASE_DIR /'media'
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from ecommerce.settings import MEDIA_ROOT
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

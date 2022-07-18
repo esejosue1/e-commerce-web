c9b3e5a0adff31b4339faf02e2c5a8a1de209d6f
josue g <josuegallardolara@hotmail.com>
2022-07-18 11:19:46 -0700

    addition of product admin

---
+++ store/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Product  # importing the Product class
+
+
+# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'price', 'stock',
+                    'category', 'modified_date', 'is_available')
+    # tuple with only one item
+    prepopulated_fields = {'slug': ('product_name',)}
+
+
+admin.site.register(Product, ProductAdmin)

aa2ea6dbfec71f51ebd732844f673f5767e102b9
josue g <josuegallardolara@hotmail.com>
2022-07-19 19:03:52 -0700

    displayed all products in the store url, looped each product, added total items found.

--- store/views.py
+++ store/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 
 
 def store(request):
-    return render(request, 'store/store.html')
+    # only filtered the available products to the home page
+    products = Product.objects.all().filter(is_available=True)
+    # pass in only the products
+    product_count = products.count()
+    context = {
+        'products': products,
+        'product_count': product_count
+    }
+    return render(request, 'store/store.html', context)

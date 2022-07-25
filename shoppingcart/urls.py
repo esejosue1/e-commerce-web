aef3bb5defb47d1d12f82ba4832db1a8836e22b9
josue g <josuegallardolara@hotmail.com>
2022-07-25 09:29:53 -0700

    Adding to shopping cart functionality implemented

--- shoppingcart/urls.py
+++ shoppingcart/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.shoppingcart, name='shoppingcart'),
+    path('', views.shoppingcart, name='shoppingcart'),  #homepage for shopping cart window
+    path('add_shoppingcart/<int:product_id>/',          #url path for when a product is added into the shopping cart
+         views.add_shoppingcart, name='add_shoppingcart'), 
 ]
--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -1,7 +1,47 @@
 # what will be shown when a request is made, to the cart html file
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from shoppingcart.models import CartItem, ShoppingCart
+from store.models import Product
 # Create your views here.
+
+# function to obtain the current scession (cookie) frmo browser
+
+
+def get_session_id(request):
+    cart = request.session.session_key  # obtain the current session
+    if not cart:
+        cart = request.session.create()  # if not, create a new one
+    return cart
+
+# function that will add products into the cart
+
+
+def add_shoppingcart(request, product_id):
+    # get the product id from Product class
+    product = Product.objects.get(id=product_id)
+    # want to check if we have a shopping cart session established
+    try:
+        cart = ShoppingCart.objects.get(
+            cart_id=get_session_id(request))  # get the current session
+    except ShoppingCart.DoesNotExist:  # else create and get new session
+        cart = ShoppingCart.objects.create(
+            cart_id=get_session_id(request)
+        )
+    cart.save()
+
+    # check what products we have in current shopping cart session
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product,
+            quantity=1,
+            cart=cart
+        )
+    cart_item.save()
+    return redirect('shoppingcart')  # redirect to shopping cart homwepage
 
 
 def shoppingcart(request):

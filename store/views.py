23aeebe3eaa5bbf91dbf8bd6e46f80d23c6903f2
josue g <josuegallardolara@hotmail.com>
2022-07-28 10:03:47 -0700

    Added existing item in shopping cart option and view cart option to the product detail html

--- store/views.py
+++ store/views.py
@@ -1,9 +1,15 @@
+from itertools import product
 from tkinter import E
+
 from django.shortcuts import get_object_or_404, render
+
 
 from category.models import Category
 from .models import Product
 from category.models import Category
+from shoppingcart.views import get_session_id, shoppingcart
+from shoppingcart.models import CartItem
+
 
 # Create your views here.
 
@@ -40,12 +46,16 @@ def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(
             category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=get_session_id(  # check in cartItems, filter throught the cart, obtaining the cart id (cart__cart_id) which will be the session id
+            request), product=single_product).exists()  # if the item is in shopping cart already, return boolean
+
     except Exception as e:
         raise e
 
     # context will hold the single product to be used in the html doc as a reference
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
 
     return render(request, 'store/product_detail.html', context)

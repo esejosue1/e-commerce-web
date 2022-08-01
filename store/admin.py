4bb971e8e278344702fe8c6583ebf3209f67350a
josue g <josuegallardolara@hotmail.com>
2022-08-01 15:03:33 -0700

    Adding variation models for admin

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product  # importing the Product class
+from .models import Product, Variation  # importing the Product class
 
 
 # Register your models here.
@@ -11,5 +11,15 @@ class ProductAdmin(admin.ModelAdmin):
     # tuple with only one item
     prepopulated_fields = {'slug': ('product_name',)}
 
+#class to display the variation option under the admin page
+class variationAdmin(admin.ModelAdmin):
+    
+    list_display = ('product', 'variation_category', 'variation_value', #what will be shown
+                    'is_active', 'created_date')
+    list_editable = ('is_active',)  #is active check box
+    list_filter = ('product', 'variation_category', 'variation_value')  #right side box that filter through the products
 
+
+# get access to he code in which the admin will add data
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, variationAdmin)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+#models use for the admin user to add data under the store category
 from django.db import models
 from category.models import Category
 from django.urls import reverse
@@ -19,11 +20,31 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
-    
-    #url path to direct to each product page through the products page, self(class product).category(//access the category under this class product)
+
+    # url path to direct to each product page through the products page, self(class product).category(//access the category under this class product)
     # .slug(//access the slug in the category section under slug, they are foreign keys)
     def get_url(self):
-        return reverse('product_detail', args=[self.category.slug,self.slug])
+        return reverse('product_detail', args=[self.category.slug, self.slug])
 
     def __str__(self):
         return self.product_name
+
+
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size'),
+)
+
+
+class Variation(models.Model):
+    # if the product gets deleted, the variation should be deleted
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(
+        max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        # return product from Variation class
+        return self.product

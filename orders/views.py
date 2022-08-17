ae1915ac4f39a961ce8a4ef519ce8c3f04e75b4d
josue g <josuegallardolara@hotmail.com>
2022-08-17 14:55:43 -0700

    Adding paypal funcitonality, obtaining the order data and passing it to the orderproduct model based on the successful order.

--- orders/views.py
+++ orders/views.py
@@ -3,16 +3,82 @@ from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
-import datetime
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
 from shoppingcart.models import CartItem
+from store.models import Product
 from .forms import OrderForm
-from .models import Order
+from .models import Order, Payment, OrderProduct
+
+import datetime
+import json
 
 
 # Create your views here.
 
 def payment(request):
+    body = json.loads(request.body)
+    order = Order.objects.get(
+        user=request.user, is_ordered=False, order_number=body['orderID'])
+
+    # store transaction details inside payment model
+    payment = Payment(
+        user=request.user,
+        payment_id=body['transID'],
+        payment_method=body['payment_method'],
+        amount_paid=order.order_total,  # accessed by the order query, total in models
+        status=body['status'],
+    )
+    payment.save()
+
+    # update and is_ordered in Order model since its a foreign key of Payment model
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+
+    print(body)
+
+    # move the cart items to Order Product table
+    cart_items = CartItem.objects.filter(user=request.user)
+    for item in cart_items:
+        order_product = OrderProduct()
+        order_product.order_id = order.id
+        order_product.payment = payment
+        order_product.user_id = request.user.id
+        order_product.product_id = item.product.id
+        order_product.quantity = item.quantity
+        order_product.product_price = item.product.price
+        order_product.is_ordered = True
+        order_product.save()
+
+        # get the variations of the cart item since we have manytomany variations
+        cart_item = CartItem.objects.get(id=item.id)
+        product_variation = cart_item.variations.all()
+        order_product = OrderProduct.objects.get(id=order_product.id)
+        order_product.variations.set(product_variation)
+        order_product.save()
+
+        # reduce the quantity of the sold prodcuts
+        product = Product.objects.get(id=item.product_id)
+        product.stock -= item.quantity
+        product.save()
+
+    # clear cart
+    CartItem.objects.filter(user=request.user).delete()
+
+    # send order received email to customer
+    mail_subject='Thank you for your purchase, items are on the way!'
+    message=render_to_string('orders/order_recieved_email.html', {
+        'user': request.user,
+        'order': order,
+    })
+    to_email=request.user.email
+    send_mail=EmailMessage(mail_subject, message, to=[to_email])
+    send_mail.send()
+
+    # send order number and transaction idback to the script as a json response
+
     return render(request, 'orders/payment.html')
 
 
@@ -49,8 +115,9 @@ def place_order(request, total=0, quantity=0):
             data.state = form.cleaned_data['state']
             data.order_note = form.cleaned_data['order_note']
             # total order
-            data.total = grand_total
+            data.order_total = grand_total
             data.tax = tax
+
             data.ip = request.META.get('REMOTE_ADDR')
 
             data.save()  # generates unique id
